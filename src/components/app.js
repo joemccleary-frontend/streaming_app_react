7de8942b172ff6b6bbc8e6a95c324cec7aa520a3
Joe McCleary <joemccleary@hotmail.co.uk>
2021-06-14 08:53:29 +0100

    added browser routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>Pageone</div>
+}
+const PageTwo = () => {
+    return <div>PageTwo
+        <button>
+            Clicky
+        </button>
+    </div>
+}
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+            <div>
+                <Route path="/" exact component={PageOne} />
+                <Route path="/pagetwo" component={PageTwo} />
+            </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
