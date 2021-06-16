9d5e78c4ca465d62a044d442b0038617cbb31458
Joe McCleary <joemccleary@hotmail.co.uk>
2021-06-16 10:24:55 +0100

    streamList class based added

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formreducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     auth: authReducer,
-    form: formreducer
+    form: formreducer,
+    streams: streamReducer,
 })
